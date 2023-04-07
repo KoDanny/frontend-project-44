ef08e5888b9cccb05ccd77d4def90eb12e37eaf6
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-04-08 00:09:11 +0300

    update. Переписал brain-even, чтобы функция сама выводила результаты работы на экран, а не возвращала их

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import getQuestionsAboutName from './cli.js';
 
@@ -9,6 +8,7 @@ const getGameBrainEven = () => {
   console.log(`${'Hello'}, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let result = '';
+  let correctAnswerCount = 0;
   for (let i = 0; i <= 2; i += 1) {
     const randomNumber = getRandomNumber();
     const question = `${'Question:'} ${randomNumber}`;
@@ -17,23 +17,31 @@ const getGameBrainEven = () => {
     if (answer.toLowerCase() === 'yes') {
       if (randomNumber % 2 === 0) {
         console.log('Correct!');
+        correctAnswerCount += 1;
       } else {
         result = (`${"'yes' is wrong answer ;(. Correct answer was 'no'."}\n${"Let's try again"}, ${name}!`);
-        return result;
+        console.log(result);
+        break;
       }
     } else if (answer.toLowerCase() === 'no') {
       if (randomNumber % 2 !== 0) {
         console.log('Correct!');
+        correctAnswerCount += 1;
       } else {
         result = (`${"'no' is wrong answer ;(. Correct answer was 'yes'."}\n${"Let's try again"}, ${name}!`);
-        return result;
+        console.log(result);
+        break;
       }
     } else {
-      return `${answer} ${"is wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again"}, ${name}!`;
+      result = `${answer} ${"is wrong answer ;(. Correct answer was 'yes' or 'no'.\nLet's try again"}, ${name}!`
+      console.log(result);
+      break;
     }
   }
-  result = `${'Congratulations'}, ${name}!`;
-  return result;
+  if (correctAnswerCount === 3) {
+    result = `${'Congratulations'}, ${name}!`;
+    console.log(result);
+  }
 };
 
 export default getGameBrainEven;
