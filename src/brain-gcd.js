59b6664e8f4c5f8200172b82631b83168d0b1bfb
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-04-14 22:06:53 +0300

    Седьмой шаг. Добавил проверку на 0

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -14,10 +14,15 @@ const getGameBrainGcd = () => {
   const rule = 'Find the greatest common divisor of given numbers.';
   console.log(rule);
   let correctAnswerCount = 0;
+  let roundCount = 3;
   let result = '';
-  for (let i = 1; i <= 3; i += 1) {
+  for (let i = 1; i <= roundCount; i += 1) {
     const firstNumber = getRandomNumber(15) * 3;
     const secondNumber = getRandomNumber(12) * 4;
+    if(firstNumber === 0 || secondNumber === 0) {
+        roundCount += 1;
+        continue;
+    }
     const answer = getQuestion(`${'Question:'} ${firstNumber} & ${secondNumber}`);
     const correctAnswer = getGreatestCommonDivisor(firstNumber, secondNumber);
     if (isCorrect(answer, correctAnswer)) {
