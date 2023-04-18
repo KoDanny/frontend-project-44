502c320bb3daef1bdb8109c998f42e38b8b1967e
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-04-18 20:53:46 +0300

    исправление ошибок

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+import getGreeting from '../src/cli.js';
 import { getPersonalGreeting } from '../src/index.js';
 
+getGreeting();
 getPersonalGreeting('May I have your name?');
