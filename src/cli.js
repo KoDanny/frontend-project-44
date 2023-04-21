2a095b4490380d9e228795f5b5220336cca6ae14
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-04-22 01:58:22 +0300

    КодКлимату не нравится хлам, переписываем

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,5 @@
-import readlineSync from 'readline-sync';
-
-const getGreeting = () => {
+const getGameGreeting = () => {
   console.log('Welcome to the Brain Games!');
-  const question = 'May I have your name?';
-  const name = readlineSync.question(`${question} `);
-  console.log(`${'Hello'}, ${name}!`);
-  return name;
 };
 
-export default getGreeting;
+export default getGameGreeting;
