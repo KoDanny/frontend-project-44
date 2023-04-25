93eed609462064f7b71755f79740a00987160889
Komogorov Denis <komogorov.denis1990@bk.ru>
2023-04-25 16:18:51 +0300

    исправление ошибки

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,7 +5,7 @@ const isPrime = (number) => {
   if (number < 2) {
     return false;
   }
-  for (let divisor = 2; divisor < Math.sqrt(number); divisor += 1) {
+  for (let divisor = 2; divisor <= Math.sqrt(number); divisor += 1) {
     if (number % divisor === 0) {
       return false;
     }
